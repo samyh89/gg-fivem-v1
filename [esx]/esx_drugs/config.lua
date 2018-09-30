fc68130ca94e164683b76208087512ea0d24f7c1
samyh89 <36846640+samyh89@users.noreply.github.com>
2018-09-30 15:32:17 +0300

    metafix

--- [esx]/esx_drugs/config.lua
+++ [esx]/esx_drugs/config.lua
@@ -20,7 +20,7 @@ Config.Zones = {
 	CokeField =			{x = -1658.01,	y = 3022.69,	z = 32.91,	name = _U('coke_field'),		sprite = 501,	color = 40},
 	CokeProcessing =	{x = 3600.66,	y = 3657.42,	z = 33.87,	name = _U('coke_processing'),	sprite = 478,	color = 40},
 	CokeDealer =		{x = 523.04,	y = 198.57,		z = 108.31,	name = _U('coke_dealer'),		sprite = 500,	color = 75},
-	MethField =			{x = 1443.36,	y = 6323.82,	z = 23.74,	name = _U('meth_field'),		sprite = 499,	color = 26},
+	MethField =			{x = 1443.36,	y = 6323.82,	z = 23.00,	name = _U('meth_field'),		sprite = 499,	color = 26},
 	MethProcessing =	{x = -1148.85,	y = -1512.89,	z = 10.63,	name = _U('meth_processing'),	sprite = 499,	color = 26},
 	MethDealer =		{x = -2259.42,	y = 233.21,		z = 174.61,	name = _U('meth_dealer'),		sprite = 500,	color = 75},
 	WeedField =			{x = -2798.43,	y = 1434.61,	z = 100.83,	name = _U('weed_field'),		sprite = 496,	color = 52},
